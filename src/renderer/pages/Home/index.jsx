4b720ef16bc20b9b67b382af50bb3c3e52ee6525
Pablo Souza <pablovsouza@gmail.com>
2023-03-03 01:02:24 +0000

    Better state management

--- src/renderer/pages/Home/index.jsx
+++ src/renderer/pages/Home/index.jsx
@@ -4,8 +4,16 @@ import ComicList from "./components/ComicList";
 import ComicDashboard from "./components/ComicDashboard";
 
 import style from "./style.module.scss";
+import useComicData from "store/comic";
+import { useMemo } from "react";
 
 const Home = () => {
+  const { resetComic } = useComicData((state) => state);
+
+  useMemo(() => {
+    // resetComic();
+  }, []);
+
   return (
     <Window className={style.Home}>
       <TopMenu />

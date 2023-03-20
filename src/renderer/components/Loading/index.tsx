92d7f2e31c8b23235d8caccc2a2916a73cba4ec6
Pablo Souza <30412188+PabloVSouza@users.noreply.github.com>
2023-03-20 03:34:00 +0000

    Convert loading

---
+++ src/renderer/components/Loading/index.tsx
@@ -0,0 +1,33 @@
+import Cover from 'components/Cover'
+import Image from 'components/Image'
+
+import useLoading from 'store/loading'
+
+import style from './style.module.scss'
+
+import imgLoading from 'assets/loading.svg'
+
+const Loading = (): JSX.Element => {
+  const state = useLoading((state) => state)
+  const { status, message, progress } = state
+
+  const totalProgress = Math.round((100 / progress.total) * progress.current)
+  return (
+    <Cover visible={status} index="999">
+      {message !== '' ? (
+        <div className={style.message}>
+          <h1>Downloading Chapter {message}</h1>
+          <Image pure src={imgLoading} alt="" />
+          <div className={style.progressBar}>
+            <p>{totalProgress}%</p>
+            <div className={style.bar} style={{ width: `${totalProgress}%` }} />
+          </div>
+        </div>
+      ) : (
+        <Image pure src={imgLoading} alt="" />
+      )}
+    </Cover>
+  )
+}
+
+export default Loading
--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import Routes from 'routes'

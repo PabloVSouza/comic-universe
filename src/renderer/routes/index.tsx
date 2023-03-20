557760067b7cc958bf7fac52be220f5ed5f61e76
Pablo Souza <30412188+PabloVSouza@users.noreply.github.com>
2023-03-20 03:30:57 +0000

    Convert routes

--- src/renderer/routes/index.tsx
+++ src/renderer/routes/index.tsx
@@ -1,11 +1,12 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { ReactElement } from 'react'
+import { Route, Routes, Navigate, RoutesProps } from 'react-router-dom'
 
-import Home from "pages/Home";
-import Download from "pages/Download";
-import Reader from "pages/Reader";
-import Users from "pages/Users";
+import Home from 'pages/Home'
+import Download from 'pages/Download'
+import Reader from 'pages/Reader'
+import Users from 'pages/Users'
 
-const Router = () => {
+const Router = (): ReactElement<RoutesProps> => {
   return (
     <Routes>
       <Route path="/" element={<Home />}>
@@ -15,7 +16,7 @@ const Router = () => {
       <Route path="/reader/:comicId/:number" element={<Reader />} />
       <Route path="/users" element={<Users />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default Router;
+export default Router

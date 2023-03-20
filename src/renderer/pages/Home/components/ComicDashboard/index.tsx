2e5695a9fa890ff6ac16f0f7ebef77eeab32a932
Pablo Souza <30412188+PabloVSouza@users.noreply.github.com>
2023-03-20 18:29:18 +0000

    Dashboard conversion

---
+++ src/renderer/pages/Home/components/ComicDashboard/index.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react'
+
+import DashboardHeader from './Header'
+import DashboardNav from './Nav'
+import DashboardList from './List'
+
+import useDashboard from 'store/dashboard'
+
+import style from './style.module.scss'
+
+const ComicDashboard = ({ item }: { item: Comic }): JSX.Element => {
+  const { getReadProgressDB, activeComic } = useDashboard((state) => state)
+
+  useMemo(() => {
+    getReadProgressDB()
+  }, [activeComic])
+
+  const { list } = useDashboard((state) => state)
+
+  return (
+    <div className={style.comicDashboard}>
+      {!!item?._id && (
+        <>
+          <DashboardHeader item={item} />
+          <DashboardNav />
+          <DashboardList list={list} />
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ComicDashboard

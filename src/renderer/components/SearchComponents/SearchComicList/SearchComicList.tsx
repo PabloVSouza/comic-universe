f45fac3bf694c19637cabba642e1fa442cab1ef7
Pablo Souza <pablovsouza@gmail.com>
2024-08-06 05:24:04 +0100

    Improve pagination logic

--- src/renderer/components/SearchComponents/SearchComicList/SearchComicList.tsx
+++ src/renderer/components/SearchComponents/SearchComicList/SearchComicList.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef, MutableRefObject } from 'react'
 import ReactPaginate from 'react-paginate'
-
 import SearchComicListItem from '../SearchComicListItem/SearchComicListItem'
 import useLang from 'lang'
 
@@ -13,6 +12,8 @@ interface SearchComicList {
 
 const SearchComicList = ({ list, itemsPerPage = 10 }: SearchComicList): JSX.Element => {
   const [offset, setOffset] = useState(0)
+  const [page, setPage] = useState(0)
+  const refScrollElement = useRef() as MutableRefObject<HTMLUListElement>
   const endOffset = offset + itemsPerPage
   const currentItems = list?.slice(offset, endOffset)
   const totalPages = Math.ceil(list.length / itemsPerPage)
@@ -20,13 +21,26 @@ const SearchComicList = ({ list, itemsPerPage = 10 }: SearchComicList): JSX.Elem
   const texts = useLang()
 
   const handlePageClick = (event): void => {
+    setPage(event.selected)
     const newOffset = (event.selected * itemsPerPage) % list.length
     setOffset(newOffset)
   }
 
+  useEffect(() => {
+    setOffset(0)
+    setPage(0)
+    const { current } = refScrollElement
+    if (current) current.scrollTop = 0
+  }, [list])
+
+  useEffect(() => {
+    const { current } = refScrollElement
+    if (current) current.scrollTop = 0
+  }, [offset])
+
   return (
     <div className={style.SearchComicList}>
-      <ul className={style.list}>
+      <ul className={style.list} ref={refScrollElement}>
         {currentItems.map((comic) => {
           return <SearchComicListItem data={comic} id={comic.siteId} key={comic.siteId} />
         })}
@@ -40,6 +54,7 @@ const SearchComicList = ({ list, itemsPerPage = 10 }: SearchComicList): JSX.Elem
         nextClassName={style.next}
         pageClassName={style.page}
         activeClassName={style.active}
+        forcePage={page}
         onPageChange={handlePageClick}
         pageRangeDisplayed={3}
         marginPagesDisplayed={3}

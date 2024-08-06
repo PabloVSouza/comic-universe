cd7c0d88825be005f98014362d4ecb8b14c5da43
Pablo Souza <pablovsouza@gmail.com>
2024-08-06 01:23:58 +0100

    Convert search to the new window

--- src/renderer/pages/PageList.tsx
+++ src/renderer/pages/PageList.tsx
@@ -1,9 +1,10 @@
 import Settings from './Settings'
 import About from './About'
 import Users from './Users'
-
+import Search from './Search'
 export default {
   About,
   Settings,
   Users,
+  Search
 }

4110a56413ab8ca02c4135e4bcbaf17de77dc2d4
Pablo Souza <pablovsouza@gmail.com>
2024-08-08 03:54:15 +0100

    Unused imports

--- src/renderer/components/HomeComponents/HomeBlankArea/HomeBlankArea.tsx
+++ src/renderer/components/HomeComponents/HomeBlankArea/HomeBlankArea.tsx
@@ -1,15 +1,8 @@
 import classNames from 'classnames'
 import style from './HomeBlankArea.module.scss'
-import Image from 'components/Image'
-
-import logo from 'assets/logo.svg'
 
 const HomeBlankArea = ({ active }: { active?: boolean }) => {
-  return (
-    <div className={classNames(style.HomeBlankArea, active ? style.Active : null)}>
-      {/* <Image src={logo} className={style.icon} alt="App Icon" /> */}
-    </div>
-  )
+  return <div className={classNames(style.HomeBlankArea, active ? style.Active : null)} />
 }
 
 export default HomeBlankArea

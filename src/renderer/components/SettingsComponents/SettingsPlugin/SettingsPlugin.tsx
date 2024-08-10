653168961ccd719eb4c74357fae9d70ec77133bf
Pablo Souza <pablovsouza@gmail.com>
2024-08-10 21:41:53 +0100

    Add dropzone

--- src/renderer/components/SettingsComponents/SettingsPlugin/SettingsPlugin.tsx
+++ src/renderer/components/SettingsComponents/SettingsPlugin/SettingsPlugin.tsx
@@ -1,9 +1,11 @@
+import Dropzone from 'components/Dropzone'
 import style from './SettingsPlugin.module.scss'
 
 const SettingsPlugin = () => {
   return (
     <div className={style.SettingsPlugin}>
       <h2>Plugin Settings</h2>
+      <Dropzone />
     </div>
   )
 }

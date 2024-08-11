a798830e5602fce87b9d920c4bf110e81aba272b
Pablo Souza <pablovsouza@gmail.com>
2024-08-11 01:42:37 +0100

    Fix path

--- src/renderer/functions/fixFilePaths.ts
+++ src/renderer/functions/fixFilePaths.ts
@@ -1,11 +1,11 @@
 import useGlobalStore from 'store/useGlobalStore'
 
 const FixFilePaths = (path: string) => {
-  const { appRunningPath, appPath } = useGlobalStore.getState()
+  const { appRunningPath } = useGlobalStore.getState()
 
   const { isDev } = window
 
-  const prefix = isDev ? appRunningPath : appPath
+  const prefix = isDev ? appRunningPath : ''
   return 'file://' + window.path.join(prefix, path)
 }
 

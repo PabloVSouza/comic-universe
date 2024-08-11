7b989b965c5530cf53635a0276ea0b494f7af08e
Pablo Souza <pablovsouza@gmail.com>
2024-08-11 02:04:50 +0100

    Styling list

--- src/renderer/components/SettingsComponents/SettingsPlugin/SettingsPluginListItem/SettingsPluginListItem.tsx
+++ src/renderer/components/SettingsComponents/SettingsPlugin/SettingsPluginListItem/SettingsPluginListItem.tsx
@@ -7,11 +7,14 @@ import style from './SettingsPluginListItem.module.scss'
 const SettingsPluginListItem = ({ plugin }: { plugin: RepoPluginInfo }) => {
   const iconPath = FixFilePaths(plugin.iconPath)
   return (
-    <li>
+    <li className={style.SettingsPluginListItem}>
       <div className={style.textArea}>
         <p className={style.name}>{plugin.name}</p>
         <span className={style.version}>{plugin.version}</span>
         <span className={style.author}>{plugin.author}</span>
+        <a href={plugin.repository} target="_blank" rel="noreferrer">
+          <u>Github Repository</u>
+        </a>
       </div>
       <Image src={iconPath} className={style.icon} />
     </li>

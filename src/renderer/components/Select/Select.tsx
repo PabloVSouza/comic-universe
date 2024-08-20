803555fb085e905fcf9c9f92a86230cfbb36fb47
Pablo Souza <pablovsouza@gmail.com>
2024-08-20 20:20:11 +0100

    Convert select

--- src/renderer/components/Select/Select.tsx
+++ src/renderer/components/Select/Select.tsx
@@ -1,6 +1,5 @@
 import ReactSelect from 'react-select'
 import classNames from 'classnames'
-import style from './Select.module.scss'
 import { StateManagerProps } from 'react-select/dist/declarations/src/useStateManager'
 
 type TSelect = {
@@ -12,13 +11,16 @@ export default function Select({ className, ...props }: TSelect) {
     <ReactSelect
       unstyled
       isSearchable={false}
+      className="[&>*]:cursor-pointer [&>*]:transition-fade"
       classNames={{
-        container: () => classNames(style.Select, className),
-        input: () => style.Input,
-        control: () => style.Control,
-        menu: () => style.Menu,
-        menuList: () => style.MenuList,
-        option: () => style.Option
+        container: () => classNames('outline-none mr-px h-full w-44 rounded', className),
+        input: () => '',
+        control: () =>
+          'pr-2 pl-5 h-full w-full overflow-hidden rounded !cursor-pointer hover:bg-list-item-hover hover:text-text-oposite',
+        menu: () => 'w-full flex pt-px bg-default rounded overflow-hidden',
+        menuList: () => 'flex flex-col w-full gap-px',
+        option: () =>
+          'bg-default w-full h-10 !flex justify-center items-center !cursor-pointer hover:bg-list-item-hover hover:text-text-oposite'
       }}
       {...props}
     />

a18c369e5bb266a4243cc21743bfba34cc8ef23b
Pablo Souza <pablovsouza@gmail.com>
2024-08-21 00:55:30 +0100

    Convert dashboard header

--- src/renderer/components/HomeComponents/HomeDashboard/HomeDashboardHeader/HomeDashboardHeader.tsx
+++ src/renderer/components/HomeComponents/HomeDashboard/HomeDashboardHeader/HomeDashboardHeader.tsx
@@ -6,8 +6,6 @@ import Image from 'components/Image/Image'
 import useDashboardStore from 'store/useDashboardStore'
 import useGlobalStore from 'store/useGlobalStore'
 
-import style from './HomeDashboardHeader.module.scss'
-
 const HomeDashboardHeader = (): JSX.Element => {
   const { appPath } = useGlobalStore()
 
@@ -24,19 +22,21 @@ const HomeDashboardHeader = (): JSX.Element => {
       )}`
 
   return (
-    <div className={style.HomeDashboardHeader}>
-      <div className={style.texts}>
-        <div className={style.title}>
-          <h1>{HtmlParser(comic.name)}</h1>
-          {!!comic.publisher && <p>{comic.publisher}</p>}
-          {!!comic.status && <p>{comic.status}</p>}
-          {!!comic.genres && <p>{!!comic.genres && JSON.parse(comic.genres).join(', ')}</p>}
+    <div className="w-full h-72 shrink-0 bg-default flex">
+      <div className="grow flex flex-col min-h-0">
+        <div className="p-2 flex items-center flex-col [&>*]:text-center">
+          <h1 className="text-2xl">{HtmlParser(comic.name)}</h1>
+          {!!comic.publisher && <p className="text-xs">{comic.publisher}</p>}
+          {!!comic.status && <p className="text-sm">{comic.status}</p>}
+          {!!comic.genres && (
+            <p className="text-sm">{!!comic.genres && JSON.parse(comic.genres).join(', ')}</p>
+          )}
         </div>
-        <div className={style.synopsis}>
-          <p>{HtmlParser(comic.synopsis)}</p>
+        <div className="flex justify-center items-center overflow-auto p-2">
+          <p className="h-full">{HtmlParser(comic.synopsis)}</p>
         </div>
       </div>
-      <Image className={style.cover} src={cover} />
+      <Image className="h-full object-fill aspect-10/16" src={cover} />
     </div>
   )
 }
